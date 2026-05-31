562ebff57eef6e4c35415a299a358a12d76bdb05
OtonariS <sanaeprj@gmail.com>
2026-05-31 16:12:20 +0900

    MatrixLayoutトレイトのコメントから不要な引数説明を削除

--- rust/src/matrix_layout.rs
+++ rust/src/matrix_layout.rs
@@ -12,7 +12,6 @@ pub trait MatrixLayout {
     /// row, colから行・列のstrideを取得する
     /// # 引数
     /// - `row`: 行番号
-    /// - `col`: 列番号
     /// - `matrix_row`: 行数
     /// - `matrix_col`: 列数
     /// # 戻り値
@@ -21,7 +20,6 @@ pub trait MatrixLayout {
 
     /// row, colから行・列のstrideを取得する
     /// # 引数
-    /// - `row`: 行番号
     /// - `col`: 列番号
     /// - `matrix_row`: 行数
     /// - `matrix_col`: 列数
